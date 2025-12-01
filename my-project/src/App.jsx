06024e9db108a5e5ba050e02fece7b16ab809e0f
Abraham.Devgit config --global user.email abrahamoluwaniyi50@gmail.com <abrahamoluwaniyi50@gmail.comgit config --global user.name Abraham.Dev>
2025-12-01 07:53:49 +0100

    `Deleted files and dependencies for a React project using Vite and Tailwind CSS`

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,7 @@
 function App () {
   return(
     <div>
-       <h1 className="text-4xl font-bold underline bg-brown">
+       <h1 className="text-[2em] font-bold underline bg-brown">
     Hello world!
   </h1>
     </div>
